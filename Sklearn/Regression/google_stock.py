ea16e43757e0cdf7b0820f54912e15a818dbeb77
ElijahCoding <cuiyeqing960904@gmail.com>
2020-10-03 22:53:43 +0300

    plot

--- Sklearn/Regression/google_stock.py
+++ Sklearn/Regression/google_stock.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import quandl
-import math
+import math, datetime
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 quandl.ApiConfig.api_key = 'K8sShFJxcC4vM9-YM-L_'
 
@@ -25,9 +29,9 @@ forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 X = np.array(df.drop('label', 1))
+X = preprocessing.scale(X)
 X = X[:-forecast_out]
 X_lately = X[-forecast_out:]
-X = preprocessing.scale(X)
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
@@ -38,3 +42,25 @@ clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_lately)
+
+# print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
