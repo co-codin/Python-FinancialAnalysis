ae02eddb1f8c474e28ec5a8a6875eddcebb707fa
ElijahCoding <cuiyeqing960904@gmail.com>
2020-10-04 16:25:50 +0300

    model selected

--- Theory/Classification/knn.py
+++ Theory/Classification/knn.py
@@ -1,9 +1,17 @@
 import numpy as np
 from sklearn import preprocessing, neighbors
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 df = pd.read_csv('./datasets/breast-cancer-wisconsin.data')
 df.replace('?', -99999, inplace=True)
 
-print(df.head())
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
