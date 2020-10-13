ad0a7938937dc375d15c1c54ca358c45712b8c11
ElijahCoding <cuiyeqing960904@gmail.com>
2020-10-13 19:15:15 +0300

    visualise price

--- Stock Analysis/money_flow_index.py
+++ Stock Analysis/money_flow_index.py
@@ -7,4 +7,13 @@ warnings.filterwarnings('ignore')
 
 df = pd.read_csv('./datasets/AAPL.csv')
 
-print(df.head())
+df = df.set_index(pd.DatetimeIndex(df['Date']))
+
+# visualisation
+plt.figure(figsize=(12.2, 4.5))
+plt.plot(df['Close'], label='Close Price')
+plt.title('Apple Close Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend(df.columns.values, loc='upper left')
+plt.show()
