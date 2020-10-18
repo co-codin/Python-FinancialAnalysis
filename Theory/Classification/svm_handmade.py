a233839f1182d23fb69fd055e0f3e0ec6d4ba36c
ElijahCoding <cuiyeqing960904@gmail.com>
2020-10-18 22:14:50 +0300

    skeleton built

--- Theory/Classification/svm_handmade.py
+++ Theory/Classification/svm_handmade.py
@@ -10,4 +10,17 @@ data_dict = {
 }
 
 class Support_Vector_Machine:
-    pass
+    def __init__(self, visualization=True):
+        self.visualization = visualization
+        self.colors = {1: 'r', -1: 'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1,1,1)
+
+    def fit(self, datta):
+        pass
+
+    def predict(self, features):
+        classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+
+        return classification
